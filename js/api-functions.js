fb03cc3e45469146f6aaf47b7e89b0bc28f81abd
lorenzo ferrario <student@epicodus.com>
2016-03-18 11:14:53 -0700

    add fieldReplace function to avoid null values

--- js/api-functions.js
+++ js/api-functions.js
@@ -2,23 +2,22 @@ var token = require('./../.env').token;
 var urlTemplate = 'https://api.github.com/users/%USER%';
 var repoTemplate = '/repos';
 var tokenTemplate = '?access_token=' + token;
-var profileTemplate = '<div class="profile"><h2>%USERNAME%</h2><h4>%NAME%</h4><img src="%URL%" alt="avatar" /><h3>%EMAIL%</h3><h3>Followers : %FOLLOWERS%</h3></div>'
+var profileTemplate = '<div class="profile"><h2>%USERNAME%</h2><h4>%NAME%</h4><img src="%URL%" alt="avatar" /><h3>Email address: %EMAIL%</h3><h3>Followers : %FOLLOWERS%</h3></div>'
 
 exports.getRepos = function(){
 };
 
+String.prototype.fieldReplace = function(substr1, substr2 ){
+  return (substr2? this.replace(substr1, substr2) : this.replace(substr1, 'Private'));
+}
+
 exports.getProfile = function(response){
   $('.container').empty();
-  var profile = profileTemplate.replace('%USERNAME%', response.login).
-  replace('%NAME%', response.name).
-  replace('%URL%', response.avatar_url).
-  replace('%FOLLOWERS%', response.followers);
-  if(response.email){
-    profile = profile.replace('%EMAIL%', response.email);
-  }
-  else {
-    profile = profile.replace('%EMAIL%', 'Private');
-  }
+  var profile = profileTemplate.fieldReplace('%USERNAME%', response.login).
+  fieldReplace('%NAME%', response.name).
+  fieldReplace('%URL%', response.avatar_url).
+  fieldReplace('%FOLLOWERS%', response.followers).
+  fieldReplace('%EMAIL%', response.email);
   $('.container').append(profile);
 };
 
